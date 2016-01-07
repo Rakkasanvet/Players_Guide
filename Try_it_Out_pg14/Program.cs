ac366a09548f0e76e8cdcf1f7093edf2112de0d4
Jason Allen Gill <black6romeo@yahoo.com>
2016-01-07 10:14:48 -0600

    Setup for GP1

---
+++ Try_it_Out_pg14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_it_Out_pg14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
