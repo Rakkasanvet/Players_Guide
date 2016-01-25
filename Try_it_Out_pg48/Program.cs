4cfadceb2f29908d1a0d682fe401a8927c0d109d
Jason Allen Gill <black6romeo@yahoo.com>
2016-01-25 11:17:53 -0600

    pg 48 update

--- Try_it_Out_pg48/Program.cs
+++ Try_it_Out_pg48/Program.cs
@@ -10,6 +10,19 @@ namespace Try_it_Out_pg48
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the radius of the clyinder");
+            var radius = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the height of the clyinder");
+            var height = Int32.Parse(Console.ReadLine());
+            float pi = 3.1415926f;
+            var Volume = (pi*radius*radius*height);
+            var surfaceArea = 2*pi*(radius + height);
+            Console.WriteLine("The clyinder's volume is"+ Volume);
+            Console.WriteLine("The surface area is"+ surfaceArea);
+            Console.ReadLine();
+
+
+
         }
     }
 }
