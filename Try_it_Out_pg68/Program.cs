48529b3a66dbbfab9629cbc262aa6d8992f40bd7
Jason Allen Gill <black6romeo@yahoo.com>
2016-01-26 14:38:17 -0600

    try it out pg68 complete

--- Try_it_Out_pg68/Program.cs
+++ Try_it_Out_pg68/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,29 @@ namespace Try_it_Out_pg68
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a number that you would like to be computed?");
+            var firstNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("please enter a second number that you would to be computed?");
+            var secondNumber = Int32.Parse(Console.ReadLine());
+
+            if (firstNumber > 0 && secondNumber > 0)
+                Console.WriteLine("The number is positive");
+
+           else if (firstNumber <0 && secondNumber <0)
+                Console.WriteLine("The number is positive");
+           else
+        
+               Console.WriteLine("The number is negative");
+        
+          
+           
+
+
+
+
+
+
+
         }
     }
 }
