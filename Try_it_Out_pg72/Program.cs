b4c2e9b92035b9fe9a07cf79dd5e62307b139507
Jason Allen Gill <black6romeo@yahoo.com>
2016-01-27 14:28:48 -0600

    questions on pg 72 cant figure it out?

--- Try_it_Out_pg72/Program.cs
+++ Try_it_Out_pg72/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,49 @@ namespace Try_it_Out_pg72
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Please begin your math Problem by entering a number?");
+            var numberOne = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the type of Mathmatical equation you would like to use? +, -, *, /, %");
+            var operation = Console.ReadLine();
+            Console.WriteLine("Please enter your second numer to have your equation computed.");
+            var secondNumber = Int32.Parse(Console.ReadLine());
+
+            var answer = Resultant(numberOne, secondNumber, operation);
+            Console.WriteLine("answer of " + numberOne + " " + operation + " " + secondNumber + " " + answer);
+        }
+
+        public static int Resultant(int numberOne, int secondNumber, string operation)
+        {
+            int Answer = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    answer = numberOne + secondNumber;
+                    break;
+                case "-":
+                    answer = numberOne - secondNumber;
+                    break;
+                case "*":
+                    answer = numberOne * secondNumber;
+                    break;
+                case "/":
+                    answer = numberOne / secondNumber;
+                    break;
+                case "%":
+                    answer = numberOne % secondNumber;
+                    break;
+
+
+            }
+            return Answer;
+        }
+           // i am as lost as last years easter egg what am i missing? this is the first one that i had to go look at your github to add some stuff i
+           // did not know that i needed please help!
+
+
         }
     }
-}
+    
